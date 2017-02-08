66f44c3cbc9adaa5edf8ed0c5dbe8ab83c636cc5
Atiyeh Rezghian <rezghian.atiyeh@gmail.com>
2017-02-09 01:59:04 +0330

    تمرین جلسه دوم

---
+++ S3C_MVC/S3C_MVC/Controllers/ShopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace S3C_MVC.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
